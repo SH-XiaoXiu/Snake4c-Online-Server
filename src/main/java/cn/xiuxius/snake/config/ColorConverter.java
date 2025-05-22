ffabe39b0b35cc8aed3ebb9a418aeeae89afcdca
XiaoXiu <sh_xiaoxiu@qq.com>
2025-05-22 22:56:58 +0800

    :bug: 修复玩家名显示问题 修复logback日志问题

--- src/main/java/cn/xiuxius/snake/config/ColorConverter.java
+++ src/main/java/cn/xiuxius/snake/config/ColorConverter.java
@@ -9,40 +9,71 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ColorConverter extends CompositeConverter<ILoggingEvent> {
-
     private static final Map<Integer, AnsiColor> LEVELS;
-
     static {
         Map<Integer, AnsiColor> map = new HashMap<>();
-        map.put(Level.ERROR_INTEGER, AnsiColor.RED);
-        map.put(Level.WARN_INTEGER, AnsiColor.MAGENTA);
-        map.put(Level.INFO_INTEGER, AnsiColor.GREEN);
-        map.put(Level.DEBUG_INTEGER, AnsiColor.BRIGHT_YELLOW);
+        map.put(Level.ERROR_INTEGER, AnsiColor.BRIGHT_RED);
+        map.put(Level.WARN_INTEGER, AnsiColor.BRIGHT_YELLOW);
+        map.put(Level.INFO_INTEGER, AnsiColor.BRIGHT_GREEN);
+        map.put(Level.DEBUG_INTEGER, AnsiColor.BRIGHT_BLACK);
         map.put(Level.TRACE_INTEGER, AnsiColor.BRIGHT_BLACK);
         LEVELS = Collections.unmodifiableMap(map);
     }
-
     @Override
     protected String transform(ILoggingEvent event, String in) {
-        String param = getFirstOption();  // 读取{}里的参数，比如 RED, BLUE 等
-
+        String param = getFirstOption();
         AnsiColor color = null;
         if (param != null) {
             color = AnsiColor.fromName(param);
         }
         if (color == null) {
-            // 参数为空时，使用日志级别对应颜色
             color = LEVELS.get(event.getLevel().toInteger());
             if (color == null) {
                 color = AnsiColor.WHITE;
             }
         }
-
         return toAnsiString(in, color);
     }
+    protected String toAnsiString(String in, AnsiColor element) {
+        return AnsiOutput.toString(element, in);
+    }
 
-    protected String toAnsiString(String in, AnsiColor color) {
-        return AnsiOutput.toString(color, in);
+    public static abstract class AnsiOutput {
+        private static final String ENCODE_JOIN = ";";
+        private static final String ENCODE_START = "\033[";
+        private static final String ENCODE_END = "m";
+        private static final String RESET = "0;" + AnsiColor.DEFAULT;
+        public static String toString(Object... elements) {
+            StringBuilder sb = new StringBuilder();
+            buildEnabled(sb, elements);
+            return sb.toString();
+        }
+        private static void buildEnabled(StringBuilder sb, Object[] elements) {
+            boolean writingAnsi = false;
+            boolean containsEncoding = false;
+            for (Object element : elements) {
+                if (element instanceof AnsiColor) {
+                    containsEncoding = true;
+                    if (!writingAnsi) {
+                        sb.append(ENCODE_START);
+                        writingAnsi = true;
+                    } else {
+                        sb.append(ENCODE_JOIN);
+                    }
+                } else {
+                    if (writingAnsi) {
+                        sb.append(ENCODE_END);
+                        writingAnsi = false;
+                    }
+                }
+                sb.append(element);
+            }
+            if (containsEncoding) {
+                sb.append(writingAnsi ? ENCODE_JOIN : ENCODE_START);
+                sb.append(RESET);
+                sb.append(ENCODE_END);
+            }
+        }
     }
 
     public enum AnsiColor {
@@ -88,5 +119,7 @@ public class ColorConverter extends CompositeConverter<ILoggingEvent> {
     }
 
 }
+
+
 
 
